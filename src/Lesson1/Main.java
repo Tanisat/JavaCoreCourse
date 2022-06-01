7034c5a36daf95bae86e87f10cdd031472ff6e15
Tanisat <atanisat@gmail.com>
2022-06-01 22:59:30 +0300

    Lesson1

--- src/Lesson1/Main.java
+++ src/Lesson1/Main.java
@@ -1,4 +1,10 @@
 package Lesson1;
 
 public class Main {
+    public static void main(String[] args) {
+        Course c = new Course(new Obstacle[]{new Cross(80), new Wall(2), new Wall(1), new Cross(120), new Water(100)});
+        Team team = new Team("Horde", new Competitor[]{new Human("Trall"), new Human("Sylvana"), new Dog("Voljiin")});
+        c.go(team);
+        team.showResults();
+    }
 }
